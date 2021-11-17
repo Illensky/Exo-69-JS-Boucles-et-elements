32ce8a2470b79a64688fb11f2eb81a2ea243dffa
Illensky <alexis.laroche.02240@gmail.com>
2021-11-17 16:19:03 +0100

    Exercice done

---
+++ exo 2/script.js
@@ -0,0 +1,15 @@
+let list = document.getElementById('liste-commissions');
+let input = document.getElementById('textInput');
+let valid = document.getElementById('validation');
+let supprimer = document.getElementById('delete');
+
+
+valid.addEventListener('click', function (){
+    let nouvelElement = document.createElement("li");
+    nouvelElement.innerText = input.value;
+    list.appendChild(nouvelElement);
+});
+
+supprimer.addEventListener("click", function (oldChild){
+    list.lastChild.remove();
+});
